11218b2cbc9bbe3be31d650e1e0f506b266f53d5
thisisaaroland <aaron@mapzen.com>
2015-10-19 18:20:41 -0700

    better docs and go fmt-ing

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # go-whosonfirst-log
+
+A simple Go package to implement level-dependent logging.
+
+## Usage
+
+```
+import (
+       "io"
+       log "github.com/whosonfirst/go-whosonfirst-log"
+)
+
+loglevel := "info"
+verbose := true
+
+logfile, err := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+
+if err != nil {
+	panic(err)
+}
+
+writer := io.MultiWriter(logfile)
+
+if verbose {
+	writer = io.MultiWriter(os.Stdout, logfile)
+	loglevel = "debug"
+}
+
+logger := log.NewWOFLogger(writer, "[your-app] ", loglevel)
+
+logger.Info("Writing all your logs to %s", logfile)
+logger.Debug("Hello world")
+```
+
+### Messages
+
+You can pass regular old `fmt.Sprintf` formats and parameters to any of the logging methods.
+ 
+### Log levels
+
+The following log levels are supported, in this order. That means if you specify that the default log level is `warning` any messages logged to the `Info` or `Debug` methods will be ignored.
+
+* debug
+* info
+* warning
+* error
+* fatal
+
+There are correspoding (public) instance methods available for each of these log levels. Invoking `logger.Fatal` will record the message to be logged and then call `os.Exit(1)`.
--- log.go
+++ log.go
@@ -1,81 +1,81 @@
 package log
 
 import (
-       golog "log"
-       "io"
-       "os"
-       "fmt"
-)      
+	"fmt"
+	"io"
+	golog "log"
+	"os"
+)
 
 type WOFLogger struct {
-     Logger    *golog.Logger
-     MinLevel  string
-     levels    map[string]int
+	Logger   *golog.Logger
+	MinLevel string
+	levels   map[string]int
 }
 
-func NewWOFLogger (out io.Writer, prefix string, minlevel string) *WOFLogger {
+func NewWOFLogger(out io.Writer, prefix string, minlevel string) *WOFLogger {
 
-     levels := make(map[string]int)
-     levels["fatal"] = 0
-     levels["error"] = 10
-     levels["warning"] = 20
-     levels["info"] = 30
-     levels["debug"] = 40
+	levels := make(map[string]int)
+	levels["fatal"] = 0
+	levels["error"] = 10
+	levels["warning"] = 20
+	levels["info"] = 30
+	levels["debug"] = 40
 
-     logger := golog.New(out, prefix, golog.Lmicroseconds)
+	logger := golog.New(out, prefix, golog.Lmicroseconds)
 
-     l := WOFLogger{Logger: logger, MinLevel: minlevel, levels:levels}
-     return &l
+	l := WOFLogger{Logger: logger, MinLevel: minlevel, levels: levels}
+	return &l
 }
 
-func (l WOFLogger) Debug (format string, v ...interface{}) {
-     l.format("debug", format, v...)          
+func (l WOFLogger) Debug(format string, v ...interface{}) {
+	l.format("debug", format, v...)
 }
 
-func (l WOFLogger) Info (format string, v ...interface{}) {
-     l.format("info", format, v...)          
+func (l WOFLogger) Info(format string, v ...interface{}) {
+	l.format("info", format, v...)
 }
 
-func (l WOFLogger) Warning (format string, v ...interface{}) {
-     l.format("warning", format, v...)          
+func (l WOFLogger) Warning(format string, v ...interface{}) {
+	l.format("warning", format, v...)
 }
 
-func (l WOFLogger) Error (format string, v ...interface{}) {
-     l.format("error", format, v...)          
+func (l WOFLogger) Error(format string, v ...interface{}) {
+	l.format("error", format, v...)
 }
 
-func (l WOFLogger) Fatal (format string, v ...interface{}) {
-     l.format("fatal", format, v...)
-     os.Exit(1)
+func (l WOFLogger) Fatal(format string, v ...interface{}) {
+	l.format("fatal", format, v...)
+	os.Exit(1)
 }
 
-func (l WOFLogger) format (level string, format string, v ...interface{}) {
+func (l WOFLogger) format(level string, format string, v ...interface{}) {
 
-     if l.emit(level){
+	if l.emit(level) {
 
-          msg := fmt.Sprintf(format, v...)
-     	  out := fmt.Sprintf("[%s] %s", level, msg)
-	  l.Logger.Println(out)
-     }	
+		msg := fmt.Sprintf(format, v...)
+		out := fmt.Sprintf("[%s] %s", level, msg)
+		l.Logger.Println(out)
+	}
 }
 
-func (l WOFLogger) emit (level string) bool {
+func (l WOFLogger) emit(level string) bool {
 
-     this_level, ok := l.levels[level]
+	this_level, ok := l.levels[level]
 
-     if ! ok {
-     	return false
-     }
+	if !ok {
+		return false
+	}
 
-     min_level, ok := l.levels[l.MinLevel]
+	min_level, ok := l.levels[l.MinLevel]
 
-     if ! ok {
-     	return false
-     }
+	if !ok {
+		return false
+	}
 
-     if this_level > min_level {
-     	return false
-     }     
+	if this_level > min_level {
+		return false
+	}
 
-     return true
+	return true
 }
